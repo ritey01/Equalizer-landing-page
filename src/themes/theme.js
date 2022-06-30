16b64270e83897b6c4ea22d0e62da6dea20da79d
Sally Wright <sallywright@developersinstitute.co.nz>
2022-06-30 20:56:33 +1200

    moved font family to index

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -8,26 +8,22 @@ const theme = {
   },
 
   h1: {
-    fontFamily: "'IBM Plex Sans', sans-serif",
     fontWeight: "700",
     fontSize: "88px",
     lineSpacing: "88px",
     letterSpacing: "-1px",
   },
   h2: {
-    fontFamily: "'IBM Plex Sans', sans-serif",
     fontWeight: "700",
     fontSize: "40px",
     lineSpacing: "52px",
   },
   body1: {
-    fontFamily: "'IBM Plex Sans', sans-serif",
     fontWeight: "400",
     fontSize: "20px",
     lineSpacing: "34px",
   },
   body2: {
-    fontFamily: "'IBM Plex Sans', sans-serif",
     fontWeight: "400",
     fontSize: "16px",
     lineSpacing: "26px",
