a717180b3de36c0ba04aab89840f7444715c4741
Sally Wright <sallywright@developersinstitute.co.nz>
2022-06-30 20:55:22 +1200

    Add images for 3 sizes

---
+++ src/components/Background.jsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { images } from "./images";
+
+const MobileImg = styled.img`
+  position: absolute;
+  top: -15%;
+  right: -25%;
+`;
+
+const TabletImg = styled.img`
+  display: none;
+`;
+
+const DesktopImg = styled.img`
+  display: none;
+`;
+
+const Background = () => {
+  return (
+    <>
+      <MobileImg
+        src={images("./bg-main-mobile.png")}
+        alt="oval-flesh coloured"
+      />
+
+      <TabletImg
+        src={images("./bg-main-tablet.png")}
+        alt="oval-flesh coloured"
+      />
+
+      <DesktopImg
+        src={images("./bg-main-desktop.png")}
+        alt="oval-flesh coloured"
+      />
+    </>
+  );
+};
+
+export default Background;
