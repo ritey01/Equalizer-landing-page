613095ca53e0dddb18a511bfa1d1b27880dccce8
Sally Wright <sallywright@developersinstitute.co.nz>
2022-06-30 20:54:35 +1200

    Add Background and container

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import Background from "./components/Background";
+import styled from "styled-components";
+
+const PageContainer = styled.div`
+  z-index: 1;
+`;
 
 const App = () => {
   return (
-    <>
+    <PageContainer>
+      <Background />
       <Header />
       <Main />
       <Footer />
-    </>
+    </PageContainer>
   );
 };
 
