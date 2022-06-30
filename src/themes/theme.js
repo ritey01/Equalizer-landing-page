759acbb89fb940e784fcf869aea7791b6529f093
Sally Wright <sallywright@developersinstitute.co.nz>
2022-06-30 15:25:06 +1200

    Add styles, color and font

---
+++ src/themes/theme.js
@@ -0,0 +1,37 @@
+const theme = {
+  colors: {
+    green: "#66E2DC",
+    darkOrange: "#FA7453",
+    lightOrange: "#FFB964",
+    white: "#66E2DC",
+    black: "#191826",
+  },
+
+  h1: {
+    fontFamily: "'IBM Plex Sans', sans-serif",
+    fontWeight: "700",
+    fontSize: "88px",
+    lineSpacing: "88px",
+    letterSpacing: "-1px",
+  },
+  h2: {
+    fontFamily: "'IBM Plex Sans', sans-serif",
+    fontWeight: "700",
+    fontSize: "40px",
+    lineSpacing: "52px",
+  },
+  body1: {
+    fontFamily: "'IBM Plex Sans', sans-serif",
+    fontWeight: "400",
+    fontSize: "20px",
+    lineSpacing: "34px",
+  },
+  body2: {
+    fontFamily: "'IBM Plex Sans', sans-serif",
+    fontWeight: "400",
+    fontSize: "16px",
+    lineSpacing: "26px",
+  },
+};
+
+export default theme;
