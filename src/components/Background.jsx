08a397475464565a5161954dff2b2d70aa94bad9
Sally Wright <sallywright@developersinstitute.co.nz>
2022-07-07 20:47:21 +1200

    Add media query, state for scree size

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,38 +1,63 @@
 import styled from "styled-components";
 import { images } from "./images";
+import { useState, useEffect } from "react";
 
 const MobileImg = styled.img`
   position: absolute;
-  top: -15%;
-  right: -25%;
-  width: 281px;
+  top: -24%;
+  right: -73%;
+  width: 30rem;
+  overflow: hidden;
+  @media (min-width: 500px) {
+    right: -50%;
+  }
 `;
 
 const TabletImg = styled.img`
-  display: none;
+  overflow: hidden;
+  position: absolute;
+  left: -42%;
+  top: -24%;
+  width: 160%;
 `;
 
 const DesktopImg = styled.img`
-  display: none;
+  overflow: hidden;
+  position: absolute;
+  left: -25%;
+  top: -25%;
+  width: 137%;
 `;
 
 const Background = () => {
+  const [width, setWidth] = useState(window.screen.width);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => setWidth(window.screen.width));
+  });
+
   return (
     <>
-      <MobileImg
-        src={images("./bg-main-mobile.png")}
-        alt="oval-flesh coloured"
-      />
+      {width < 750 && (
+        <MobileImg
+          src={images("./bg-main-mobile.png")}
+          alt="oval-flesh coloured"
+        />
+      )}
 
-      <TabletImg
-        src={images("./bg-main-tablet.png")}
-        alt="oval-flesh coloured"
-      />
+      {width >= 750 && width < 1430 && (
+        <TabletImg
+          src={images("./bg-main-tablet.png")}
+          alt="oval-flesh coloured"
+        />
+      )}
 
-      <DesktopImg
-        src={images("./bg-main-desktop.png")}
-        alt="oval-flesh coloured"
-      />
+      {width >= 1430 && (
+        <DesktopImg
+          src={images("./bg-main-desktop.png")}
+          alt="oval-flesh coloured"
+        />
+      )}
     </>
   );
 };
