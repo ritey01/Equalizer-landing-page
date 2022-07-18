2d58059f53811e516ac0c954a63d5c816ecc59fa
Sally Wright <sallywright@developersinstitute.co.nz>
2022-07-18 21:56:06 +1200

    Removed unused code

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import Background from "./components/Background";
-import styled from "styled-components";
-
-const PageContainer = styled.div``;
 
 const App = () => {
   return (
-    <PageContainer>
+    <>
       <Background />
       <Header />
       <Main />
       <Footer />
-    </PageContainer>
+    </>
   );
 };
 
